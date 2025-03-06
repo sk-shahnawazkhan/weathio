76db9f277ca5707c7423094930da5a2895eac283
Shahnawaz Khan <1990.shahnawazkhan@gmail.com>
2025-03-07 00:17:46 +0530

    added global time based on IST and sunrise/sunset

--- src/components/Weather.jsx
+++ src/components/Weather.jsx
@@ -1,12 +1,14 @@
 import React, { useEffect, useState } from "react";
+import { convertUnixToLocalTime } from "../utils/dateTime";
 
 const Weather = () => {
   const [search, setSearch] = useState("");
-  const [weatherData, setWeatherData] = useState({});
+  const [weatherData, setWeatherData] = useState(null);
   const [isButtonDisabled, setIsButtonDisabled] = useState(true);
+  const [hasError, setHasError] = useState(false);
 
   useEffect(() => {
-    getWeatherDetails("Mecca");
+    getWeatherDetails("Amroha");
   }, []);
 
   const APP_ID = import.meta.env.VITE_APP_ID;
@@ -20,8 +22,10 @@ const Weather = () => {
       setWeatherData(data);
       setSearch("");
       setIsButtonDisabled(true);
+      setHasError(false);
     } catch (error) {
       console.log(error);
+      setHasError(true);
     }
   };
 
@@ -35,20 +39,20 @@ const Weather = () => {
   };
 
   return (
-    <main className="min-h-screen flex items-center justify-center bg-linear-to-bl from-sky-300 to-purple-200">
-      <div className="p-10 w-full md:w-1/2 xl:w-1/3 bg-gradient-to-r from-sky-300 via-indigo-300 to-blue-300 bg-stone-100 rounded-xl">
+    <main className="min-h-screen bg-linear-to-bl from-sky-300 to-purple-200">
+      <section className="px-10 lg:px-20 xl:px-40 pt-10 pb-5">
         <div className="flex items-center justify-center gap-1">
           <input
             type="search"
             name="search"
             id="search"
-            placeholder="Search city or your place"
-            className="w-full h-15 text-xl border-1 border-slate-500 p-2 rounded-lg bg-white"
+            placeholder="Type place name..."
+            className="w-full sm:max-w-2/3 lg:max-w-1/2 h-15 text-center text-xl placeholder:text-gray-500 placeholder:italic placeholder:text-center border-1 border-slate-500 p-2 rounded-lg bg-slate-100"
             value={search}
             onChange={handleChange}
           />
           <button
-            className={`py-1 px-4 h-15 text-xl border-1 border-slate-500 rounded-lg bg-white text-sky-500 ${
+            className={`py-1 px-4 h-15 text-xl border-1 border-slate-500 rounded-lg bg-slate-100 text-sky-500 ${
               isButtonDisabled
                 ? "cursor-not-allowed opacity-50"
                 : "cursor-pointer"
@@ -59,37 +63,120 @@ const Weather = () => {
             Search
           </button>
         </div>
-        <div className="mt-5">
-          <p className="text-5xl font-bold text-center">
-            {weatherData?.main?.temp}°c
-          </p>
-          <p className="text-center font-semibold mt-2">
-            {weatherData?.name}
-            <span>, {weatherData?.sys?.country}</span>
-          </p>
-          <div className="mt-5 flex items-center justify-center gap-5">
-            <img
-              src={
-                weatherData && weatherData.weather && weatherData.weather[0]
-                  ? `https://openweathermap.org/img/wn/${weatherData?.weather[0]?.icon}@2x.png`
-                  : ""
-              }
-              alt=""
-              className="size-30"
-            />
-            <p className="capitalize">
-              {weatherData && weatherData.weather && weatherData.weather[0]
-                ? weatherData.weather[0].description
-                : ""}
-            </p>
+      </section>
+      <section className="px-10 lg:px-20 min-xl:px-40 py-5 weather-report">
+        <div className="grid grid-cols-2 gap-5">
+          <div className="border-1 border-slate-400">
+            {weatherData && (
+              <>
+                <div className="p-5 bg-gradient-to-l from-sky-300 via-indigo-200 to-blue-200">
+                  {weatherData.name && (
+                    <h2 className="text-xl text-slate-600 font-semibold">
+                      {weatherData?.name}, {weatherData?.sys?.country} |{" "}
+                      <span className="text-lg text-slate-500">
+                        {convertUnixToLocalTime(
+                          weatherData.dt,
+                          weatherData.timezone
+                        )}{" "}
+                        IST
+                      </span>
+                    </h2>
+                  )}
+                </div>
+                <div className="p-5 bg-gradient-to-r from-sky-200 via-indigo-200 to-blue-300">
+                  <div className="flex items-center justify-between">
+                    <div className="flex flex-col justify-center gap-2">
+                      <h2 className="text-7xl font-bold text-center text-slate-600">
+                        {Math.floor(weatherData?.main?.temp)}°c
+                      </h2>
+                      <div className="flex items-center justify-between">
+                        <p>↑ {Math.floor(weatherData?.main?.temp_max)}°c</p>
+                        <p>↓ {Math.floor(weatherData?.main?.temp_min)}°c</p>
+                      </div>
+                    </div>
+                    <div className="flex flex-col items-center">
+                      <img
+                        src={
+                          weatherData &&
+                          weatherData.weather &&
+                          weatherData.weather[0]
+                            ? `https://openweathermap.org/img/wn/${weatherData?.weather[0]?.icon}@2x.png`
+                            : ""
+                        }
+                        alt=""
+                        className="size-20"
+                      />
+                      <p className="capitalize">
+                        {weatherData &&
+                        weatherData.weather &&
+                        weatherData.weather[0]
+                          ? weatherData.weather[0].description
+                          : ""}
+                      </p>
+                    </div>
+                  </div>
+                  <div className="my-5">
+                    Feels Like{" "}
+                    <span className="text-5xl font-semibold text-slate-600">
+                      {Math.floor(weatherData?.main?.feels_like)}°c
+                    </span>
+                  </div>
+                  <div className="lg:grid grid-cols-2 gap-x-10 gap-y-2">
+                    <div className="flex items-center justify-between border-b-1 border-slate-400 pb-2">
+                      <p>Humidity</p>
+                      <span>{weatherData?.main?.humidity}%</span>
+                    </div>
+                    <div className="flex items-center justify-between border-b-1 border-slate-400 pb-2">
+                      <p>Wind Speed</p>
+                      <span>{weatherData?.wind?.speed} km/h</span>
+                    </div>
+                    <div className="flex items-center justify-between border-b-1 border-slate-400 pb-2">
+                      <p>Pressure</p>
+                      <span>
+                        {weatherData?.main?.pressure > 1013.25
+                          ? `↑ ${weatherData?.main?.pressure}`
+                          : `↓ ${weatherData?.main?.pressure}`}{" "}
+                        hPa
+                      </span>
+                    </div>
+                    <div className="flex items-center justify-between border-b-1 border-slate-400 pb-2">
+                      <p>Visibility</p>
+                      <span>{weatherData?.visibility / 1000} km</span>
+                    </div>
+                    <div className="flex items-center justify-between max-lg:border-b-1 max-lg:border-slate-400 max-lg:pb-2">
+                      <p>Sunrise</p>
+                      <span>
+                        {convertUnixToLocalTime(
+                          weatherData.sys.sunrise,
+                          weatherData.timezone
+                        )}
+                      </span>
+                    </div>
+                    <div className="flex items-center justify-between">
+                      <p>Sunset</p>
+                      <span>
+                        {convertUnixToLocalTime(
+                          weatherData.sys.sunset,
+                          weatherData.timezone
+                        )}
+                      </span>
+                    </div>
+                  </div>
+                </div>
+              </>
+            )}
+            {hasError && (
+              <div className="mt-5">
+                <p className="text-center text-red-500">
+                  ❌ Something went wrong. Please try again later.
+                </p>
+              </div>
+            )}
           </div>
 
-          <div className="mt-5 flex items-center justify-between">
-            <p>Humidity: {weatherData?.main?.humidity}%</p>
-            <p>Wind Speed: {weatherData?.wind?.speed} km/h</p>
-          </div>
+          {/* Map will be here... */}
         </div>
-      </div>
+      </section>
     </main>
   );
 };
---
+++ src/utils/dateTime.jsx
@@ -0,0 +1,39 @@
+export const convertUnixToLocalTime = (unixTimestamp, timezoneSeconds) => {
+  const date = new Date(unixTimestamp * 1000);
+  return new Intl.DateTimeFormat("en-GB", {
+    // year: "numeric",
+    // month: "long",
+    // day: "numeric",
+    hour: "2-digit",
+    minute: "2-digit",
+    // second: "2-digit",
+    hour12: true,
+    timeZone: getTimeZoneName(timezoneSeconds),
+  }).format(date);
+};
+
+const getTimeZoneName = (timezoneSeconds) => {
+  const timeZones = {
+    0: "UTC", // Coordinated Universal Time
+    19800: "Asia/Kolkata", // IST (India Standard Time, UTC+5:30)
+    18000: "Asia/karachi", // IST (Pakistan Standard Time, UTC+5:00)
+    20700: "Asia/Kathmandu", // NPT (Nepal Time, UTC+5:45)
+    21600: "Asia/Dhaka", // BST (Bangladesh Time, UTC+6)
+    25200: "Asia/Jakarta", // WIB (Western Indonesia Time, UTC+7)
+    28800: "Asia/Shanghai", // CST (China Standard Time, UTC+8)
+    32400: "Asia/Tokyo", // JST (Japan Standard Time, UTC+9)
+    36000: "Australia/Sydney", // AEST (Australian Eastern Standard Time, UTC+10)
+    12600: "Asia/Tehran", // IRST (Iran Standard Time, UTC+3:30)
+    14400: "Asia/Dubai", // GST (Gulf Standard Time, UTC+4)
+    10800: "Europe/Moscow", // MSK (Moscow Time, UTC+3)
+    7200: "Europe/Berlin", // CET (Central European Time, UTC+2)
+    3600: "Europe/London", // BST (British Summer Time, UTC+1)
+    "-18000": "America/New_York", // EST (Eastern Standard Time, UTC-5)
+    "-21600": "America/Chicago", // CST (Central Standard Time, UTC-6)
+    "-25200": "America/Denver", // MST (Mountain Standard Time, UTC-7)
+    "-28800": "America/Los_Angeles", // PST (Pacific Standard Time, UTC-8)
+    "-32400": "America/Anchorage", // AKST (Alaska Standard Time, UTC-9)
+    "-36000": "Pacific/Honolulu", // HST (Hawaii Standard Time, UTC-10)
+  };
+  return timeZones[timezoneSeconds.toString()] || "UTC"; // Convert timezoneSeconds to string for lookup
+};
