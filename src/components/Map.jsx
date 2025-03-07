a70d682d85a3c1f421265f0c3d8d6e2bf3e57a3e
Shahnawaz Khan <1990.shahnawazkhan@gmail.com>
2025-03-08 01:26:14 +0530

    resolved --markerBuildError

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -1,9 +1,16 @@
 import React from "react";
+import L from "leaflet";
 import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
 import "leaflet/dist/leaflet.css";
+import markerIconPng from "leaflet/dist/images/marker-icon.png";
+import markerShadowPng from "leaflet/dist/images/marker-shadow.png";
 
 const Map = ({ lat, long, name }) => {
   const position = [lat, long];
+  const markerIcon = L.icon({
+    iconUrl: "/marker-icon.png",
+    shadowUrl: "marker-shadow.png",
+  });
 
   return (
     <MapContainer
@@ -17,7 +24,7 @@ const Map = ({ lat, long, name }) => {
         // attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
       />
 
-      <Marker position={position}>
+      <Marker position={position} icon={markerIcon}>
         <Popup className="text-sm text-slate-500 italic">
           📍 {name}, You are here!
         </Popup>
